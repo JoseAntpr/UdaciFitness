e1bfed05f1e4ad1a35b1d2728735145a61e65c99
Alex Sánchez <alxsnchez@gmail.com>
2017-11-13 01:44:14 +0100

    style: slider, stepper and date header

--- components/FitnessSlider.js
+++ components/FitnessSlider.js
@@ -1,14 +1,28 @@
 import React from 'react'
-import { View, Slider, Text } from 'react-native'
+import { View, Slider, Text, StyleSheet } from 'react-native'
+import { gray } from '../utils/colors';
 
 const FitnessSlider = ({ max, unit, step, value, onChange}) => (
-    <View>
-        <Slider step={step} value={value} maximumValue={max} minimumValue={0} onValueChange={onChange}/>
-        <View>
-            <Text>{value}</Text>
-            <Text>{unit}</Text>
+    <View style={styles.row}>
+        <Slider step={step} value={value} maximumValue={max} minimumValue={0} onValueChange={onChange} style={{flex: 1}}/>
+        <View style={styles.metricCounter}>
+            <Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
+            <Text style={{fontSize: 18, color: gray}}>{unit}</Text>
         </View>
     </View>
 )
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center'
+    },
+    metricCounter: {
+        width: 85,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 
 export default FitnessSlider
